387c9cbc0f52953f0f4d92508b14f39164e00083
Sergei S <sergeis@MacBook-Air-Sergei.local>
2026-07-14 16:42:01 +0500

    Add proto file for inventory and update inventory cmd.

--- inventory/cmd/main.go
+++ inventory/cmd/main.go
@@ -1,89 +1,100 @@
 package main
 
 import (
+	"context"
+	"maps"
+	"slices"
 	"sync"
-	"time"
 
-	"github.com/google/uuid"
+	inventoryv1 "github.com/massodo1993/service-example/shared/pkg/proto/inventory/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-type Part struct {
-	uuid           uuid.UUID
-	name           string
-	description    string
-	price          float32
-	stock_quantity int64
-	category       Category
-	dimensions     Dimensions
-	manufacturer   Manufacturer
-	tags           []string
-	metadata       map[string]Value
-	created_at     time.Time
-	updated_at     time.Time
+type inventoryService struct {
+	inventoryv1.UnimplementedInventoryServiceServer
+
+	mu    sync.Mutex
+	parts map[string]*inventoryv1.Part
 }
 
-type Category int
+func (is *inventoryService) GetPart(_ context.Context, request *inventoryv1.GetPartRequest) (*inventoryv1.GetPartResponse, error) {
+	is.mu.Lock()
+	defer is.mu.Unlock()
 
-const (
-	C_UNKNOWN Category = iota
-	C_ENGINE
-	C_FUEL
-	C_PORTHOLE
-	C_WING
-)
-
-func (c Category) String() string {
-	switch c {
-	case 1:
-		return "UNKNOWN"
-	case 2:
-		return "ENGINE"
-	case 3:
-		return "FUEL"
-	case 4:
-		return "PORTHOLE"
-	case 5:
-		return "WING"
-	default:
-		return "UNKNOWN"
+	part, empty := is.parts[request.GetUuid()]
+	if empty {
+		return nil, status.Errorf(codes.NotFound, "Part with UUID %s not found", request.GetUuid())
 	}
+
+	return &inventoryv1.GetPartResponse{
+		Part: part,
+	}, nil
 }
 
-type Dimensions struct {
-	length float64
-	width  float64
-	height float64
-	weight float64
-}
-
-type Manufacturer struct {
-	name    string
-	country string
-	website string
-}
-
-type Value struct {
-	string_value string
-	int64_value  int64
-	double_value float64
-	bool_value   bool
-}
-
-func NewPart() Part {
-	return Part{
-		uuid: uuid.New(),
+func (is *inventoryService) ListParts(_ context.Context, request *inventoryv1.ListPartsRequest) (*inventoryv1.ListPartsResponse, error) {
+	is.mu.Lock()
+	defer is.mu.Unlock()
+	result := maps.Clone(is.parts)
+	if isEmpty(request.Filter) {
+		parts := mapToSlice(result)
+		return &inventoryv1.ListPartsResponse{Parts: parts}, nil
 	}
-}
 
-type PartStorage struct {
-	mu   sync.RWMutex
-	part map[int]*Part
-}
-
-func NewPartStorage() *PartStorage {
-	return &PartStorage{
-		part: make(map[int]*Part),
+	if len(request.Filter.GetUuids()) > 0 {
+		maps.DeleteFunc(result, func(uuid string, _ *inventoryv1.Part) bool {
+			return !slices.Contains(request.Filter.GetUuids(), uuid)
+		})
 	}
+	if len(request.Filter.GetNames()) > 0 {
+		maps.DeleteFunc(result, func(_ string, value *inventoryv1.Part) bool {
+			return !slices.Contains(request.Filter.GetNames(), value.Name)
+		})
+	}
+	if len(request.Filter.GetCategories()) > 0 {
+		maps.DeleteFunc(result, func(_ string, value *inventoryv1.Part) bool {
+			return !slices.Contains(request.Filter.GetCategories(), value.Category)
+		})
+	}
+	if len(request.Filter.GetManufacturerCountries()) > 0 {
+		maps.DeleteFunc(result, func(_ string, value *inventoryv1.Part) bool {
+			return !slices.Contains(request.Filter.GetManufacturerCountries(), value.Manufacturer.String())
+		})
+	}
+	if len(request.Filter.GetTags()) > 0 {
+		maps.DeleteFunc(result, func(_ string, value *inventoryv1.Part) bool {
+			for _, tag := range value.GetTags() {
+				if slices.Contains(request.Filter.GetTags(), tag) {
+					return false
+				}
+			}
+			return true
+		})
+	}
+
+	parts := mapToSlice(result)
+	return &inventoryv1.ListPartsResponse{
+		Parts: parts,
+	}, nil
+}
+
+func mapToSlice(m map[string]*inventoryv1.Part) []*inventoryv1.Part {
+	result := make([]*inventoryv1.Part, 0, len(m))
+	for _, v := range m {
+		result = append(result, v)
+	}
+	return result
+}
+
+func isEmpty(f *inventoryv1.PartsFilter) bool {
+	if f == nil {
+		return true
+	}
+	return len(f.GetUuids()) == 0 &&
+		len(f.GetNames()) == 0 &&
+		len(f.GetCategories()) == 0 &&
+		len(f.GetManufacturerCountries()) == 0 &&
+		len(f.GetTags()) == 0
 }
 
 func main() {
